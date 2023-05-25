edf5468b81aea2bfc54c4ddba76194aa83000ca4
Ikbal Hukumdar <99831078+IkbalHukumdar@users.noreply.github.com>
2023-05-25 21:58:49 +0300

    ekleme ve silme işlemleri yapıldı, ErrorModal componenti oluşturuldu, Error state yönetimi sağlandı

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import WorkerList from "./components/UI/WorkerList";
+import { useState } from "react";
 import AddWorker from "./components/Workers/AddWorker";
+import WorkerList from "./components/Workers/WorkerList";
 
 function App() {
-  return <div className="App">
-    <h1 className="text-white text-center mt-6 text-3xl">Maaş Otomasyonu</h1>
-   <AddWorker/> 
-   <WorkerList workers={[]}/>
-  </div>;
+  const [workers, setWorkers] = useState([]);
+  return (
+    <div className="App">
+      <h1 className="text-white text-center mt-6 text-3xl">Maaş Otomasyonu</h1>
+      <AddWorker setWorkers={setWorkers} />
+      <WorkerList workers={workers} setWorkers={setWorkers} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -2,8 +2,9 @@ const Button = (props) => {
   return (
     <button
       className={`p-2 text-lg bg-teal-700 text-white ${props.className}`}
-      type ={props.type|| "button"}
-      >
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
