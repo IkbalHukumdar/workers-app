16e8c978caebb3b3a3b5a6d1ab2bd3a56f56cfd3
Ikbal Hukumdar <99831078+IkbalHukumdar@users.noreply.github.com>
2023-05-25 20:11:54 +0300

    state yönetimi,stateleri resetleme ve validation işlemleri gerçekleştirildi.

--- src/components/Workers/AddWorker.jsx
+++ src/components/Workers/AddWorker.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 
 const AddWorker = () => {
+  const [enteredWorkerName, setEnteredWorkerName] = useState("");
+  const [enteredWage, setEnteredWage] = useState("");
+
+  const minimumWage = 5000;
+
+  const addWorkerHandler = (e) => {
+    e.preventDefault();
+    if (
+      enteredWorkerName.trim().length === 0 ||
+      enteredWage.trim().length === 0
+    ) {
+      return;
+    }
+// + koymamızın sebebi String ifade ile başladığı için number olmasını istediğimiz için.
+    if (+enteredWage < minimumWage) {
+      return;
+    } 
+
+    setEnteredWorkerName("");
+    setEnteredWage("");
+    console.log(enteredWorkerName, enteredWage);
+  };
+
   return (
     <Card className="mt-10">
-      <form className="flex flex-col gap-y-2">
+      <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
         <label htmlFor="name" className="font-medium">
           Çalışan İsmi
         </label>
@@ -13,6 +37,8 @@ const AddWorker = () => {
           className="max-w-[40rem] w-full mx-auto border p-2"
           placeholder="Çalışan ismi yazınız"
           id="name"
+          onChange={(e) => setEnteredWorkerName(e.target.value)}
+          value={enteredWorkerName} //inputlara veri girdikten sonra boşaltmak için
         />
         <label htmlFor="wage" className="font-medium">
           Maaş Miktarı
@@ -22,8 +48,10 @@ const AddWorker = () => {
           className="max-w-[40rem] w-full mx-auto border p-2"
           placeholder="Maaş miktarı yazınız"
           id="wage"
+          onChange={(e) => setEnteredWage(e.target.value)}
+          value={enteredWage}
         />
-        <Button className="mt-2">
+        <Button className="mt-2" type="submit">
           Ekle
         </Button>
       </form>
