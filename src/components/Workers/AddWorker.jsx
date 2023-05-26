eb57c055b0f96b02e0e6848d613fb8f612da58b2
Ikbal Hukumdar <99831078+IkbalHukumdar@users.noreply.github.com>
2023-05-26 13:44:56 +0300

    wrapper componenti oluşturuldu,react fragment kullanıldı

--- src/components/Workers/AddWorker.jsx
+++ src/components/Workers/AddWorker.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
+import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
@@ -43,7 +44,7 @@ const AddWorker = (props) => {
     setError(null);
   };
   return (
-    <div>
+    <Fragment>
       {error && <ErrorModal onConfirm={errorHandler} error={error} />}
       <Card className="mt-10">
         <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
@@ -74,7 +75,39 @@ const AddWorker = (props) => {
           </Button>
         </form>
       </Card>
-    </div>
+    </Fragment>
+    /*     <>
+      {error && <ErrorModal onConfirm={errorHandler} error={error} />}
+      <Card className="mt-10">
+        <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
+          <label htmlFor="name" className="font-medium">
+            Çalışan İsmi
+          </label>
+          <input
+            type="text"
+            className="max-w-[40rem] w-full mx-auto border p-2"
+            placeholder="Çalışan ismi yazınız"
+            id="name"
+            onChange={(e) => setEnteredWorkerName(e.target.value)}
+            value={enteredWorkerName}
+          />
+          <label htmlFor="wage" className="font-medium">
+            Maaş Miktarı
+          </label>
+          <input
+            type="number"
+            className="max-w-[40rem] w-full mx-auto border p-2"
+            placeholder="Maaş miktarı yazınız"
+            id="wage"
+            onChange={(e) => setEnteredWage(e.target.value)}
+            value={enteredWage}
+          />
+          <Button className="mt-2" type="submit">
+            Ekle
+          </Button>
+        </form>
+      </Card>
+    </> */
   );
 };
 
